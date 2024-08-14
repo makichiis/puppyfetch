08903d73779a9029615e5648386b9a4aa7b3d100
midirhee12 <midirhee@proton.me>
2024-08-14 02:24:32 +0100

    fix(flake): remove buildPhase

--- nix/package.nix
+++ nix/package.nix
@@ -2,18 +2,13 @@
 let
   pname = "puppyfetch";
   version = "0.1.0";
-  cflags = "-Wall -Wextra -O2 -Wno-unused-result -Wno-unused-parameter";
 in
 stdenv.mkDerivation {
   inherit pname version;
   src = ../.;
 
-  buildPhase = ''
-    gcc ${cflags} puppyfetch.c
-  '';
-
   installPhase = ''
     mkdir -p $out/bin
-    cp a.out $out/bin/puppyfetch
+    cp puppyfetch $out/bin/puppyfetch
   '';
 }
